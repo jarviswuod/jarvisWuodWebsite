a824a20ff2a60a58c7a2eba02e1de17d6c7d8b8e
jarviswuod <johnsonmakings@gmail.com>
2025-04-05 17:12:57 +0300

    Added images and rotation aspect to Home Hero section

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,29 @@
-// "use strict";
+"use strict";
+
+// HOME HERO CARD SLIDER
+const cards = document.querySelectorAll(".home-hero-card");
+const section = document.querySelector('section[style*="background-image"]');
+
+const newImageUrl = [
+  "static/hero-img.webp",
+  "static/hero-img2.webp",
+  "static/hero-img3.webp",
+];
+
+let index = 0;
+
+function rotateContent(i) {
+  cards.forEach((card, idx) => {
+    card.classList.toggle("hidden", idx !== i);
+  });
+  section.style.backgroundImage = `url('${newImageUrl[i]}')`;
+}
+
+rotateContent(index);
+setInterval(() => {
+  index = (index + 1) % cards.length;
+  rotateContent(index);
+}, 10000);
 
 // TWITTER TESTIMONIAL SLIDER
 let current = 0;
