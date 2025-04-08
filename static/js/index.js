eadd674a2d9b9f78ed210fa5452e81da0142b75d
jarviswuod <johnsonmakings@gmail.com>
2025-04-08 09:28:31 +0300

    Updated the contact form dynamics

--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,29 @@
 "use strict";
 
-// HOME HERO CARD SLIDER
-const cards = document.querySelectorAll(".home-hero-card");
-const section = document.querySelector('section[style*="background-image"]');
+// // HOME HERO CARD SLIDER
+// const cards = document.querySelectorAll(".home-hero-card");
+// const section = document.querySelector(".home-hero-section");
 
-const newImageUrl = [
-  "static/hero-img.webp",
-  "static/hero-img2.webp",
-  "static/hero-img3.webp",
-];
+// const newImageUrl = [
+//   "static/hero-img.webp",
+//   "static/hero-img2.webp",
+//   "static/hero-img3.webp",
+// ];
 
-let index = 0;
+// let index = 0;
 
-function rotateContent(i) {
-  cards.forEach((card, idx) => {
-    card.classList.toggle("hidden", idx !== i);
-  });
-  section.style.backgroundImage = `url('${newImageUrl[i]}')`;
-}
+// function rotateContent(i) {
+//   cards.forEach((card, idx) => {
+//     card.classList.toggle("hidden", idx !== i);
+//   });
+//   section?.style.backgroundImage = `url('${newImageUrl[i]}')`;
+// }
 
-rotateContent(index);
-setInterval(() => {
-  index = (index + 1) % cards.length;
-  rotateContent(index);
-}, 10000);
+// rotateContent(index);
+// setInterval(() => {
+//   index = (index + 1) % cards.length;
+//   rotateContent(index);
+// }, 10000);
 
 // TWITTER TESTIMONIAL SLIDER
 let current = 0;
@@ -44,3 +44,32 @@ function prevSlide() {
 }
 
 showSlide(current); // init
+
+// //////////////////////////////////////////////////////////
+// CONTACT FORM MODAL IN CONTACT PAGE
+// //////////////////////////////////////////////////////////
+const contactFormModal = document.querySelector(".contact-modal");
+const contactOptions = document.querySelector(".contact-options");
+
+const contactFormModalClose = document.querySelector(".close-btn");
+const contactFormModalOverlay = document.querySelector(".modal-overlay");
+
+contactOptions.addEventListener("click", () => {
+  contactFormModal.classList.remove("hidden");
+});
+
+contactFormModalClose.addEventListener("click", () => {
+  contactFormModal.classList.add("hidden");
+});
+
+contactFormModalOverlay.addEventListener("click", () => {
+  contactFormModal.classList.add("hidden");
+});
+
+function showFormSection(section) {
+  const forms = document.querySelectorAll(".form--contact");
+  forms.forEach((form) => form.classList.add("hidden"));
+
+  const formToShow = document.getElementById(`${section}-form`);
+  formToShow.classList.remove("hidden");
+}
