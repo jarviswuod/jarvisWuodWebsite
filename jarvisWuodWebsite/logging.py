07ef035a31bcd75f94c85cc962a393edd4b6380d
jarviswuod <johnsonmakings@gmail.com>
2025-06-27 19:01:36 +0300

    Loggin error fix in production

--- jarvisWuodWebsite/logging.py
+++ jarvisWuodWebsite/logging.py
@@ -6,6 +6,8 @@ from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGS_DIR = BASE_DIR / 'logs'
+LOGS_DIR.mkdir(exist_ok=True)
 
 LOGGING = {
     "version": 1,
